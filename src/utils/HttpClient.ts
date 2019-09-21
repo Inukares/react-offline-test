da009ed03472b53c225d84e9bd9de7df75cc5247
Piotr Żaczek <zelowski@gmail.com>
2019-09-21 15:11:51 +0200

    added HttpClient and tests for it

---
+++ src/utils/HttpClient.ts
@@ -0,0 +1,18 @@
+import Axios, { AxiosResponse } from "axios";
+
+// a lot more could be added there, like other methods (e.g. POST), some actual logging when err happens etc.
+export default class HttpClient {
+
+  public get = async <T>(endpoint: string): Promise<T> => new Promise(
+    async (resolve, reject): Promise<void> => {
+      try {
+        const res: AxiosResponse = await Axios.get<T>(endpoint);
+        resolve(res.data);
+      } catch (error) {
+        const err = error.response ? error.response.data : error;
+        reject(err);
+      }
+    },
+  )
+
+}
---
+++ src/utils/__tests__/HttpClient.test.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+import HttpClient from "../HttpClient";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>; // support for autocomplete
+
+const mockSuccessFetch = (returnedData: unknown) => mockedAxios.get.mockResolvedValueOnce({
+  data: returnedData,
+  status: 200,
+});
+
+const mockRejectedFetch = (data: unknown) => mockedAxios.get.mockRejectedValueOnce({
+  response: {
+    data,
+  },
+  status: 400,
+});
+
+describe("HttpClient", () => {
+  const API = new HttpClient();
+
+  const mockData = {
+    data: "someImportantThing",
+    status: 200,
+  };
+
+  const mockError = {
+    response: {
+      data: "some data inside error",
+    },
+  };
+
+  const mockEmptyError = {
+    error: "Uncaught Exception",
+  };
+
+  it("Should perform GET request", async () => {
+    mockSuccessFetch(mockData);
+    const data = await API.get("/successful-fetch")
+    expect(data).toBe(mockData);
+  });
+
+  it("Given error with data should reject with data", async () => {
+    mockRejectedFetch(mockError);
+
+    try {
+      await API.get("/failed-fetch");
+    } catch (error) {
+      expect(error).toBe(mockError)
+    }
+  });
+
+  it("Given error without data should reject with error object", async () => {
+    mockRejectedFetch(mockEmptyError)
+
+    try {
+      await API.get("/failed-operation"); // should think of better name probably
+    } catch (error) {
+      expect(error).toBe(mockEmptyError);
+    }
+  });
+});
